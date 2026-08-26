a241a881f0bb9b8a163b426be892064d460dfbb1
Dmytro <kylon03@gmail.com>
2026-08-26 16:15:02 +0300

    feat: add promo timer end state

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,12 +1,26 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const deadline = "2026-09-1";
+  const deadline = "2026-9-1";
 
   function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-      days = Math.floor(t / (1000 * 60 * 60 * 24)),
-      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-      minutes = Math.floor((t / 1000 / 60) % 60),
-      seconds = Math.floor((t / 1000) % 60);
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    if (t <= 0) {
+      const el = document.querySelectorAll(".timer__block");
+      const tim = document.querySelector(".timer");
+      for (let i = 0; i < el.length; i++) {
+        el[i].remove();
+      }
+      let div = document.createElement("div");
+      div.innerHTML = "Акція закінчилась!";
+      div.classList.add("timer__over");
+      tim.append(div);
+    } else {
+      ((days = Math.floor(t / (1000 * 60 * 60 * 24))),
+        (hours = Math.floor((t / (1000 * 60 * 60)) % 24)),
+        (minutes = Math.floor((t / 1000 / 60) % 60)),
+        (seconds = Math.floor((t / 1000) % 60)));
+    }
+
     return {
       total: t,
       days: days,
